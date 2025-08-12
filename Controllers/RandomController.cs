52b44b68eff06ae6016c8b4c68a5214e9f1c56c6
MgKyaw <mgkyaw20112@gmail.com>
2025-08-12 21:01:43 +0700

    feat: Add new controller

---
+++ Controllers/RandomController.cs
@@ -0,0 +1,36 @@
+[Route("api/[controller]")]
+[ApiController]
+public class RandomController : ControllerBase
+{
+    private static ILogger<RandomController> _logger;
+    
+    public RandomController(ILogger<RandomController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public void Get()
+    {
+        _logger.LogInformation("Requested a Random API");
+        int count;
+        try
+        {
+            for (count = 0; count <= 5; count++)
+            {
+                if (count == 3)
+                {
+                    throw new Exception("Random Exception Occured");
+                }
+                else
+                {
+                    _logger.LogInformation("Iteration Count is {iteration}", count);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Exception Caught");
+        }
+    }
+}
